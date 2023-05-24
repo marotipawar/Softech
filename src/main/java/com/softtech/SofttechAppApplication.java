2a476f9da7b8c4b693b60f04312d7cc2b1772422
maroti Pawar <pawarmw@gmail.com>
2023-05-24 10:50:20 +0530

    close #1

---
+++ src/main/java/com/softtech/SofttechAppApplication.java
@@ -0,0 +1,13 @@
+package com.softtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SofttechAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SofttechAppApplication.class, args);
+	}
+
+}
