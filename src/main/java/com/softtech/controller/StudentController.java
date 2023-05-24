6e5cc33a9ce551e0329f321fbf92909994d2c085
maroti Pawar <pawarmw@gmail.com>
2023-05-24 12:41:52 +0530

    Rest Controller created. closes #9

---
+++ src/main/java/com/softtech/controller/StudentController.java
@@ -0,0 +1,28 @@
+package com.softtech.controller;
+
+import com.softtech.model.Student;
+import com.softtech.services.StudentServices;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class StudentController {
+
+    private StudentServices studentServices;
+
+    public StudentController(StudentServices studentServices) {
+        this.studentServices = studentServices;
+    }
+
+    @GetMapping(path = {"/student/{id}"})
+    public Student findStudent(@PathVariable Integer id){
+        return studentServices.findStudent(id);
+    }
+    @GetMapping(path = {"/students"})
+    public List<Student> findAStudents(){
+        return studentServices.findAllStudent();
+    }
+}
